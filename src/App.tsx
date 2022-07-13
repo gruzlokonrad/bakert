2049fd4ad739ba8bcd17b5ef8c559e370f8f2001
Konrad Gruzlo <gruzlokonrad@gmail.com>
2022-07-13 21:11:07 +0200

    add nav & splash

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,12 @@
-import React from 'react';
-import './App.scss';
-
-import { MdSettingsSuggest } from 'react-icons/md'
-import logo from './img/logo.webp';
-
-
+import Nav from './components/Nav/Nav';
+import styles from './App.module.scss';
+import Splash from 'components/Splash/Splash';
 
 function App() {
   return (
-    <div className="App ">
-      <div className="logo center">
-        <img src={logo} className="logo__icon" alt="logo" />
-      </div>
-      <div className="container center">
-        <main className="">
-          <div className="content center">
-            bakert.pl <br/>
-            Pilotaż pojazdu nienormatywnego
-          </div>
-          <div className="icon center">
-              <MdSettingsSuggest />
-          </div>
-          <div className="center">
-            Strona w budowie
-          </div>
-        </main>
-      </div>
+    <div className={styles.App}>
+      <Nav />
+      <Splash />
     </div>
   );
 }
